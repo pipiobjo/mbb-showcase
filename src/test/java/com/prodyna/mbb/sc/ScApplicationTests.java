9c72a27069f8e90413bc221bc86559f4359ec967
Björn Pipiorke <net.bjoern.pipiorke@mhp.com>
2019-05-16 22:24:29 +0200

    fix - remove embedded neo4j deps

--- src/test/java/com/prodyna/mbb/sc/ScApplicationTests.java
+++ src/test/java/com/prodyna/mbb/sc/ScApplicationTests.java
@@ -6,13 +6,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Transaction;
-import org.neo4j.harness.ServerControls;
-import org.neo4j.harness.TestServerBuilders;
-
-import static org.neo4j.graphdb.Label.label;
-
 
 /**
  * @TODO make simple integration test to ensure container start is possible
