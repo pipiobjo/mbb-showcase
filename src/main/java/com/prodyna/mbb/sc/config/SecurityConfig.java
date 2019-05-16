7f8ae3dea5fb1fa0d44794680c9b989d1be4c4b3
Björn Pipiorke <net.bjoern.pipiorke@mhp.com>
2019-05-16 16:49:15 +0200

    fix: swagger bug

---
+++ src/main/java/com/prodyna/mbb/sc/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.prodyna.mbb.sc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(users.username("user").password("password").roles("USER").build());
+        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN", "ACTUATOR").build());
+        return manager;
+
+    }
+}
+
