a5d23f6229e490f1f71ca4742330a2353c16919d
Kai Kugler <kai.kugler@prodyna.com>
2019-05-16 17:44:10 +0200

    Added Controllers

--- src/main/java/com/prodyna/mbb/sc/episode/controller/EpisodeController.java
+++ src/main/java/com/prodyna/mbb/sc/episode/controller/EpisodeController.java
@@ -1,7 +1,38 @@
 package com.prodyna.mbb.sc.episode.controller;
 
+import com.prodyna.mbb.sc.actor.service.ActorDomainObject;
+import com.prodyna.mbb.sc.episode.service.EpisodeDomainObject;
+import com.prodyna.mbb.sc.episode.service.EpisodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EpisodeController {
+
+    private final EpisodeService episodeService;
+
+    @Autowired
+    public EpisodeController(EpisodeService episodeService) {
+        this.episodeService = episodeService;
+    }
+
+    @RequestMapping("/episode")
+    public Page<EpisodeDomainObject> findAll(@RequestParam Integer page, @RequestParam Integer size) {
+        return episodeService.findAll(PageRequest.of(page, size));
+    }
+
+    @RequestMapping("/episode/{id}")
+    public EpisodeDomainObject findById(@PathVariable Long id) {
+        return episodeService.findById(id);
+    }
+
+    @RequestMapping("/episode/{name}")
+    public Page<EpisodeDomainObject> findByName(@PathVariable String name, @RequestParam Integer page, @RequestParam Integer size) {
+        return episodeService.findByName(name, PageRequest.of(page, size));
+    }
 }
--- src/main/java/com/prodyna/mbb/sc/series/controller/SeriesController.java
+++ src/main/java/com/prodyna/mbb/sc/series/controller/SeriesController.java
@@ -1,7 +1,37 @@
 package com.prodyna.mbb.sc.series.controller;
 
+import com.prodyna.mbb.sc.series.service.SeriesDomainObject;
+import com.prodyna.mbb.sc.series.service.SeriesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SeriesController {
+
+    private final SeriesService seriesService;
+
+    @Autowired
+    public SeriesController(SeriesService seriesService) {
+        this.seriesService = seriesService;
+    }
+
+    @RequestMapping("/series")
+    public Page<SeriesDomainObject> findAll(@RequestParam Integer page, @RequestParam Integer size) {
+        return seriesService.findAll(PageRequest.of(page, size));
+    }
+
+    @RequestMapping("/series/{id}")
+    public SeriesDomainObject findById(@PathVariable Long id) {
+        return seriesService.findById(id);
+    }
+
+    @RequestMapping("/series/{name}")
+    public Page<SeriesDomainObject> findByName(@PathVariable String name, @RequestParam Integer page, @RequestParam Integer size) {
+        return seriesService.findByName(name, PageRequest.of(page, size));
+    }
 }
